e1a8a3c6a6b7c3f960e7465b448beec30a55cee8
NITHIN R <nithin.tej8@gmail.com>
2026-09-18 00:13:11 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (32.44%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -10,7 +10,7 @@ class Solution {
 
 
         while (i >= 0 && s.charAt(i) != ' ') {
-            length++; //  
+            length++; 
             i--;
         }
         return length; //      
